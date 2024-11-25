0a6c71813ab2aec50881c9a04de43452d7741d21
Kalina Valeva <51493780+kaliVa36@users.noreply.github.com>
2024-11-25 02:53:29 -0800

    - creating container
    - adding Constants
    - adding Extensions
    - trying to create a binary file

---
+++ FS/FileExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Constants;
+
+namespace ExtensionMethods
+{
+    public static class FileExtensions 
+    {
+        public static void ReadFilePath(this string? path) 
+        { 
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            if (Path.Exists(path))
+            {
+                BinaryWriter bwStream = 
+                    new BinaryWriter(new FileStream(path + ConstantsValues.ContainerName, FileMode.Create));
+            }
+            else 
+            {
+                throw new Exception("Location does not exist"); // mocked for testing 
+            }
+        }
+    }
+}
--- FS/Program.cs
+++ FS/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using ExtensionMethods;
+
+namespace FileSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading the container's location
+            Console.WriteLine("Enter location for storing the container:");
+            string path = Console.ReadLine();
+            path.ReadFilePath();
+
+        }
+}
+}
